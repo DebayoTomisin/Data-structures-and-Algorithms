15a28d075702e9a6df9e3f3451a3a7108970f49e
Debayo Tomisin <debayotomisin07@gmail.com>
2021-06-16 01:54:41 -0700

    movig with py

--- ground.py
+++ ground.py
@@ -51,10 +51,72 @@ class ParameterPass:
         G.append(5)
 
 
-rit = [1, 4]
-tee = rit
-tee.append(8)
-print(tee)
-print(rit)
+class ListMethods(object):
+    
+    @classmethod
+    def sumArray(cls, arr):
+        size = len(arr)
+        total = 0
+        index = 0
+        for i in range(size):
+            total += total + arr[i]
+            i += 1
+        return total
 
-u = 8
+    @classmethod
+    def sequentialSearch(cls, arr, value):
+        size = len(arr)
+        for i in range(size):
+            print(arr[i])
+            if value == arr[i]:
+                return True
+            i += 1
+        return False
+
+    @classmethod
+    def binarySearch(cls, arr, value):
+        size = len(arr)
+        low = 0
+        high = size - 1
+        while low <= high:
+            mid = int((low + high) / 2)
+            if arr[mid] == value:
+                return 'Present'
+            else:
+                if arr[mid] < value:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+        return 'not found'
+
+    @classmethod
+    def rotateArray(cls, arr, k):
+        n = len(arr)
+        cls.reverseArray(arr, 0, k-1)
+        cls.reverseArray(arr, k, n-1)
+        cls.reverseArray(arr, 0, n-1)
+        return arr
+
+    @classmethod
+    def reverse(cls, arr, start, end):
+        value = arr[start: end: -1]
+        return value
+
+    @classmethod
+    def reverseArray(cls, arr, start, end):
+        i = start
+        j = end
+        while i <= j:
+            temp = arr[i]
+            arr[i] = arr[j]
+            arr[j] = temp
+            i += 1
+            j -= 1
+        return arr
+
+
+print(ListMethods.sumArray([1, 4, 5, 10, 8, 9]))
+print(ListMethods.sequentialSearch([1, 4, 5, 9], 4))
+print(ListMethods.binarySearch([1, 5, 6, 10, 17, 20, 26], 33))
+print(ListMethods.rotateArray([1, 2, 3, 6, 7, 9, 10, 11, 34], 3))
+print(ListMethods.reverseArray([1, 3, 4, 6, 8], 1, 3))
