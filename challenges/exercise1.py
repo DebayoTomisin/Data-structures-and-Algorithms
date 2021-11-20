38d23c63bab479888b001f4ba9505b697eae74f5
Debayo Tomisin <debayotomisin07@gmail.com>
2021-11-20 12:32:45 -0800

    leetcode submissions

--- challenges/exercise1.py
+++ challenges/exercise1.py
@@ -377,3 +377,56 @@ def EditChecker(item1, item2):
 
 print('')
 print(EditChecker('aplee', 'apled'))
+
+
+'''You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+
+On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
+
+Find and return the maximum profit you can achieve.'''
+
+
+def MaximumProft2(arr):
+    maxprofit = 0
+    for i in range(len(arr) - 1):
+        if arr[i + 1] > arr[i]:
+            maxprofit += arr[i + 1] - arr[i]
+    return maxprofit
+
+
+print('')
+print(MaximumProft2([7, 1, 5, 3, 6, 4]))
+
+
+'''You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ '''
+def MaximumProfit1(arr):
+    highest = 0
+    for i in range(len(arr) - 1):
+        for j in range(i, len(arr)):
+            current = arr[j] - arr[i]
+            if current > highest:
+                highest = current
+    return highest
+
+
+def optimizedProfit1(arr):
+    minprice = arr[0]
+    maxprofit = 0
+    for i in range(len(arr)):
+        if arr[i] < minprice:
+            minprice = arr[i]
+        elif arr[i] - minprice > maxprofit:
+            maxprofit = arr[i] - minprice
+    return maxprofit
+
+
+print('')
+print(optimizedProfit1([1, 2]))
+print('')
+print(MaximumProfit1([7, 1, 5, 3, 6, 4]))
