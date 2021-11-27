f112797fbf2be2a4cbe62338a17389e0348a7edb
Debayo Tomisin <debayotomisin07@gmail.com>
2021-11-27 09:30:17 -0800

    leet code exercise of finding the words to be formed from characters in string

--- leetcode.py
+++ leetcode.py
@@ -19,3 +19,55 @@ def reverseVowels(string):
 
 print(reverseVowels('hello'))
 print(reverseVowels('programming'))
+
+"""You are given an array of strings words and a string chars.
+
+A string is good if it can be formed by characters from chars (each character can only be used once).
+
+Return the sum of lengths of all good strings in words"""
+
+
+def GoodStrings(words, char):
+    count = 0
+    for word in words:
+        if len(word) <= len(char):
+            word = list(word)
+            word_ = list(set(word))
+            print('this is the set')
+            print(word_)
+            print('this is the list')
+            print(word)
+            x = True
+            for letter in word_:
+                if (letter not in char) or word.count(letter) > char.count(letter):
+                    x = False
+                    break
+            if x:
+                count += len(word)
+    return count
+
+
+def GoodStings_(words, chars):
+    count = 0
+    for i in words:
+        if len(i) <= len(chars):
+            l = list(i)
+            l1 = list(set(l))
+            x = True
+            for k in l1:
+                if (k not in chars) or (l.count(k) > chars.count(k)):
+                    x = False
+                    break
+
+            if (x == True):
+                count += len(i)
+    return (count)
+
+
+words = ["hello", "world", "leetcode"]
+chars = "welldonehoneyr"
+
+print('')
+
+print(GoodStrings(words, chars))
+print(GoodStings_(words, chars))
