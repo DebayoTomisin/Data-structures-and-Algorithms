da9089d8503aa9b88080f0cf0d0aebe6c7d9cfc8
Debayo Tomisin <debayotomisin07@gmail.com>
2022-06-20 11:45:16 +0100

    implement permuatable string

--- challenges/lists&Arrays.py
+++ challenges/lists&Arrays.py
@@ -45,4 +45,39 @@ def PythonPackageUniqueString(str):
         return "String is not unique"
 
 
-print(PythonPackageUniqueString("abbbh"))
+"""This problem suggests that we find if a string is a permutation of another string"""
+
+NO_OF_CHARS = 256
+
+
+def arePermutation(str1, str2):
+
+    # Create a count array and initialize all
+    # values as 0
+    count = [0 for i in range(NO_OF_CHARS)]
+    i = 0
+
+    # For each character in input strings,
+    # increment count in the corresponding
+    # count array
+    while str1[i] and str2[i]:
+
+        count[str1[i]] += 1
+        count[str2[i]] -= 1
+
+    # If both strings are of different length.
+    # Removing this condition  will make the
+    # program fail for strings like "aaca" and
+    # "aca"
+    if str1[i] or str2[i]:
+        return False
+
+    # See if there is any non-zero value in
+    # count array
+    for i in range(NO_OF_CHARS):
+        if count[i]:
+            return False
+    return True
+
+
+print(arePermutation("string", "string"))
